b2e4a64696ad68a42708e33940f987a82ca9976d
kento <kento@toriwatari.bird>
2021-07-25 16:04:32 +0900

    WIP #27 Install package

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import "./src/global.scss"
 import "prismjs/themes/prism-tomorrow.css"
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 /*
 // custom typefaces
--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          `gatsby-remark-code-titles`,
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
