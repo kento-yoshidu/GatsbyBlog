02c4d574c5c30aa2fe84105d1381998f84cf7819
kento <kento@toriwatari.bird>
2021-07-27 21:13:00 +0900

    #33 Update CreatePageInfo.js

--- src/components/pageInfo.js
+++ src/components/pageInfo.js
@@ -1,11 +1,13 @@
 import React from "react"
 
+import * as Styles from "../styles/pageinfo.module.scss"
+
 const PageInfo = ({ currentPage, postCount, pageCount }) => {
 	return (
-		<>
-			<div>{currentPage} / {pageCount}</div>
-			<div>{postCount}県の記事</div>
-		</>
+    <div class={Styles.countInfo}>
+      <p className={Styles.page}><span>{postCount}</span> 件の記事</p>
+      <p className={Styles.page}><span>{pageCount}</span> ページ中 / <span>{currentPage}</span> ページ目</p>
+    </div>
 	)
 }
 
