a9b0834123358be218887cd7208ef0e737841bd1
kento <kento@toriwatari.bird>
2021-08-08 22:14:42 +0900

    Update pages.js

--- src/components/pageInfo.js
+++ src/components/pageInfo.js
@@ -4,7 +4,7 @@ import * as Styles from "../styles/pageinfo.module.scss"
 
 const PageInfo = ({ currentPage, postCount, pageCount }) => {
 	return (
-    <div class={Styles.countInfo}>
+    <div className={Styles.countInfo}>
       <p className={Styles.page}><span>{postCount}</span> 件の記事</p>
       <p className={Styles.page}><span>{pageCount}</span> ページ中 / <span>{currentPage}</span> ページ目</p>
     </div>
