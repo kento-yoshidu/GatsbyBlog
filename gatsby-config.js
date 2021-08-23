afed3084c3b39efb8f72606f90bf64c2ba30f5f7
kento <kento@toriwatari.bird>
2021-08-23 14:54:02 +0900

    Install Package

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,15 @@ module.exports = {
           },
           `gatsby-remark-code-titles`,
           {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              offsetY: `30`,
+              icon: false,
+              className: `custom-class`,
+              maintainCase: true,
+            },
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               showLineNumbers: true,
