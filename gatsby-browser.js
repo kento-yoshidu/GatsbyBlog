dc360e8d04bb293d573e88b33d3354b0bf75c075
kento <kento@toriwatari.bird>
2021-10-02 10:53:28 +0900

    🛠 prismjsの読み込み場所変更

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,1 @@
 import "./src/scss/global.scss"
-import "prismjs/themes/prism-tomorrow.css"
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
-
-/*
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-// normalize CSS across browsers
-import "./src/normalize.css"
-// custom CSS styles
-
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
-
-*/
