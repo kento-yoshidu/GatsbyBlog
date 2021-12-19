eb9b0621d237a1de237a9bcd3f92bcd8bdcc488b
kento <kento@toriwatari.bird>
2021-12-19 20:03:13 +0900

    🏃‍♂️ Update SamplePage

--- lib/intersectionObserver.ts
+++ lib/intersectionObserver.ts
@@ -1,21 +1,24 @@
+/*
+*  /src/templates/blog-post.tsxにて、
+*  ToCのスクロール追従を実行
+*/
 import React, { useRef } from "react"
 
 import * as Styles from "../src/styles/tableOfContent.module.scss"
 
 const IO = () => {
-	// 今回の交差を監視する要素
-	const boxes = document.querySelectorAll(".main > h1, h2, h3");
-
-  console.log(boxes)
+	// 交差を監視する要素
+	const boxes = document.querySelectorAll(".main > h1, h2, h3")
 
 	const options = {
-		root: null, // 今回はビューポートをルート要素とする
+		root: null, // ビューポートをルート要素に設定
 		rootMargin: "-30% 0px", // ビューポートの中心を判定基準にする
-		threshold: 0 // 閾値は0
+		threshold: 0,
 	};
 
 	const observer = new IntersectionObserver(doWhenIntersect, options)
-	// それぞれのboxを監視する
+
+	// それぞれのboxを監視し、交差するとdoWhenIntersect関数を呼び出す
 	boxes.forEach(box => {
 		observer.observe(box);
 	})
@@ -24,7 +27,6 @@ const IO = () => {
 	 * 交差したときに呼び出す関数
    */
 	function doWhenIntersect(entries: IntersectionObserverEntry[]) {
-    console.log(entries)
 		// 交差検知をしたもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
 		entries.forEach(entry => {
 			if (entry.isIntersecting) {
@@ -33,7 +35,7 @@ const IO = () => {
 		})
 	}
 
-	function activateIndex(element: Event["target"]) {
+	function activateIndex(element) {
     console.log(element)
 		// すでにアクティブになっている目次を選択
 		const currentActiveIndex = document.querySelector(`.${Styles.tableOfContent} .${Styles.active}`);
