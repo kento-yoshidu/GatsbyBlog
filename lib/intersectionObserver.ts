c1b201f372f42c7fa08687bd0b4194ee2a21be0e
kento <kento@toriwatari.bird>
2021-12-19 13:14:49 +0900

    🎨 CSS微修正

--- lib/intersectionObserver.ts
+++ lib/intersectionObserver.ts
@@ -14,29 +14,26 @@ const IO = () => {
 		threshold: 0 // 閾値は0
 	};
 
-	const observer = new IntersectionObserver(doWhenIntersect, options);
+	const observer = new IntersectionObserver(doWhenIntersect, options)
 	// それぞれのboxを監視する
 	boxes.forEach(box => {
 		observer.observe(box);
-	});
+	})
 
 	/**
 	 * 交差したときに呼び出す関数
    */
-	function doWhenIntersect(entries) {
+	function doWhenIntersect(entries: IntersectionObserverEntry[]) {
+    console.log(entries)
 		// 交差検知をしたもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
 		entries.forEach(entry => {
 			if (entry.isIntersecting) {
-				activateIndex(entry.target);
+				activateIndex(entry.target)
 			}
-		});
+		})
 	}
 
-	/**
-	 * 目次の色を変える関数
-	 * @param element
-   */
-	function activateIndex(element) {
+	function activateIndex(element: Event["target"]) {
     console.log(element)
 		// すでにアクティブになっている目次を選択
 		const currentActiveIndex = document.querySelector(`.${Styles.tableOfContent} .${Styles.active}`);
@@ -48,7 +45,7 @@ const IO = () => {
 		const links = document.querySelectorAll(`.tableOfContent a`);
 
     Array.from(links).map(link => {
-      if(element.innerHTML === link.innerHTML) {
+      if(element?.innerHTML === link.innerHTML) {
         link?.classList.add(Styles.active);
       }
     })
