fd63f8c13915a5faa4c4de09efd162e3616d0bc3
kento <kento@toriwatari.bird>
2022-01-08 17:12:09 +0900

    🏃‍♂️ タグのページネーションを実装

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -34,28 +34,58 @@ const Pagination: React.VFC<Props> = ({
 			nextButton,
 			nationLinks
 
-      {/*
 	prevButton = 
 		<>
-			{!isFirst && (
-				<Link
-					className={Styles.prev}
-					to={
-						currentPage === 2
-							? `/page/1/`
-							: `/page/${currentPage - 1}/`
-					}
-					rel="prev"
-				>
-					<FontAwesomeIcon icon={faChevronCircleLeft} />
-					<span>Prev</span>
-				</Link>
+			{!isFirst &&
+        !tag && !series && (
+          <Link
+            className={Styles.prev}
+            to={
+              currentPage === 2
+                ? `/page/1/`
+                : `/page/${currentPage - 1}/`
+            }
+            rel="prev"
+          >
+            <FontAwesomeIcon icon={faChevronCircleLeft} />
+            <span>Prev</span>
+          </Link>
+			)}
+			{!isFirst &&
+        tag && !series && (
+          <Link
+            className={Styles.prev}
+            to={
+              currentPage === 2
+                ? `/tag/${tag}/page/1/`
+                : `/tag/${tag}/page/${currentPage - 1}/`
+            }
+            rel="prev"
+          >
+            <FontAwesomeIcon icon={faChevronCircleLeft} />
+            <span>Prev</span>
+          </Link>
+			)}
+			{!isFirst &&
+        !tag && series && (
+          <Link
+            className={Styles.prev}
+            to={
+              currentPage === 2
+                ? `/series/${series}/page/1/`
+                : `/series/${series}/page/${currentPage - 1}/`
+            }
+            rel="prev"
+          >
+            <FontAwesomeIcon icon={faChevronCircleLeft} />
+            <span>Prev</span>
+          </Link>
 			)}
 		</>
 	
 	nextButton =
 		<>
-			{!isLast && (
+			{!isLast && !tag && !series && (
 				<Link
 					className={Styles.next}
 					to={`/page/${currentPage + 1}/`}
@@ -64,8 +94,25 @@ const Pagination: React.VFC<Props> = ({
           <FontAwesomeIcon icon={faChevronCircleRight} />
 				</Link>
 			)}
+			{!isLast && tag && !series && (
+				<Link
+					className={Styles.next}
+					to={`/tag/${tag}/page/${currentPage + 1}/`}
+				>
+					<span>Next</span>
+          <FontAwesomeIcon icon={faChevronCircleRight} />
+				</Link>
+			)}
+			{!isLast && !tag && series && (
+				<Link
+					className={Styles.next}
+					to={`/series/${series}/page/${currentPage + 1}/`}
+				>
+					<span>Next</span>
+          <FontAwesomeIcon icon={faChevronCircleRight} />
+				</Link>
+			)}
 		</>
-      */}
 
 	nationLinks =
 		<div className={Styles.nationLinks}>
@@ -86,16 +133,13 @@ const Pagination: React.VFC<Props> = ({
 								}
 								{
 									tag && !series &&
-									<Link to={`${tag}/page/${i + 1}/`}>
+									<Link to={`/tag/${tag}/page/${i + 1}/`}>
 										{i + 1}
 									</Link>
 								}
 								{
 									!tag && series &&
-                    <Link
-                      to={`/series/${series}/page/${i + 1}/`}
-                      key={`series${series}`}
-                    >
+                    <Link to={`/series/${series}/page/${i + 1}/`}>
                       {i + 1}
                     </Link>
 								}
